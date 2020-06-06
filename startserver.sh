0a69847277a2cb6d6902d6b1425797e368771940
EriolEandur <eriol_eandur@gmx.de>
2020-06-06 21:59:07 +0200

    update startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-
+counter="0"
+echo "$(date): Server started." > reboot.log
 while true
   do
-    java -Xmx4G -jar spigot-1.12.2.jar 
-#    echo start
-	sleep 1
-	if [ -e plugins/MCME-Connect/restart.nfo ] 
-	  then
-	    rm plugins/MCME-Connect/restart.nfo  
-	  else
+    java -Dname=dev -Xms5G -Xmx5G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Xloggc:gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=1M -jar paper.jar
+#    java -Dname=dev -Xms20G -Xmx20G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Xloggc:gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=1M -jar paper.jar
+    sleep 1
+    if ! [ -e plugins/MCME-Connect/restart.nfo ] 
+      then
+        echo "$(date): Server stopped." >> reboot.log
 	    break
+	  else
+        counter=$(expr $counter + 1)
+		if [ -e plugins/MCME-Connect/connectPlugin.nfo ]
+		  then
+		    echo "$(date): Restart no. $counter. Reboot by MCME-Connect plugin." >> reboot.log
+		  else
+            echo "$(date): Restart no. $counter. Reboot after server shutdown (did it crash?)." >> reboot.log
+	    fi
 	fi
 done
